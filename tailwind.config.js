c02dc29d6de4ef68e36178f293862cdebf7baccd
VicoTriansyahNasril <142963459+VicoTriansyahNasril@users.noreply.github.com>
2023-10-03 14:45:54 +0700

    Update tailwind.config.js
    Menambahkan deklarasi warna dan radius border

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+
+      borderRadius: {
+        'lg': '2rem',
+      },
+
+      colors: {
+        'krem': '#FCE192',
+        'coklat': '#E8B266',
+        'coklat-kuning': '#CF9647',
+      },
+    },
   },
   plugins: [],
 }
