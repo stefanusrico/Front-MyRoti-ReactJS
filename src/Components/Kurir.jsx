ab6deed8a45a2f3b9877a2e8b20a6200d9e0ed66
Aryagara Kristandy <72059961+kristandy@users.noreply.github.com>
2023-10-17 20:41:33 +0700

    mengubah lebar dan posisitabel

--- src/Components/Kurir.jsx
+++ src/Components/Kurir.jsx
@@ -5,7 +5,7 @@ import axios from "axios"
 import { useTable } from "react-table";
 
 function Kurir({ columns, data }) {
-  const [card, setCards] = useState([]);
+  const [card, setData] = useState([]);
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     useTable({
       columns,
@@ -28,17 +28,17 @@ function Kurir({ columns, data }) {
   return (
     <>
     <NavKoor />
-      <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center items-center">
-        <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 pt-4">
+      <div className="md:mt-4 bg-gray-100 text-gray-900">
+        <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8 pt-4">
         <div className="-my-2 overflow-x-auto -mx-4 sm:-mx-6 lg:-mx-8">
-          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="md:p-10 md:ml-60 md:mt-7 border-2 border-gray-400">
             <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-md">
-              <table {...getTableProps()} className="min-w-full divide-y divide-gray-900">
+              <table {...getTableProps()} className="divide-y divide-gray-900 w-full ml-auto mr-4">
                 <thead className="dark:bg-gray-800">
                   {headerGroups.map((headerGroup) => (
                     <tr {...headerGroup.getHeaderGroupProps()}>
                       {headerGroup.headers.map((column) => (
-                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider" {...column.getHeaderProps()}>{column.render("Header")}</th>
+                        <th scope="col" className="px-16 py-6 text-left text-base font-medium text-white uppercase tracking-wider" {...column.getHeaderProps()}>{column.render("Header")}</th>
                       ))}
                     </tr>
                   ))}
@@ -49,7 +49,7 @@ function Kurir({ columns, data }) {
                     return (
                       <tr {...row.getRowProps()} className="odd:bg-gray-700 even:bg-gray-800 text-white hover:bg-gray-600 hover:cursor-pointer focus:outline-none">
                         {row.cells.map((cell) => {
-                          return <td className="px-6 py-4 whitespace-nowrap" {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+                          return <td className="px-16 py-6 whitespace-nowrap" {...cell.getCellProps()}>{cell.render("Cell")}</td>;
                         })}
                       </tr>
                     );
--- src/dummydata.jsx
+++ src/dummydata.jsx
@@ -12,6 +12,7 @@ const getData = () => [
     role: "Admin",
     imgUrl:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    action: <div><button className="text-sky-500 hover:text-sky-400">Edit</button>  <button className="text-sky-500 hover:text-sky-400">Delete</button></div>,
   },
   {
     id: 2,
